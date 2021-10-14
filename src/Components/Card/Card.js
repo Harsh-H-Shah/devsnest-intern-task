60023ce143765e546d145e7db87c3fc4c5c663ed
harsh <harshmkvviv@gmail.com>
2021-10-14 18:02:25 +0530

    Add todo list

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { MoreHorizontal } from 'react-feather';
+
+import Dropdown from '../Dropdown/Dropdown';
+
+import './Card.css';
+import CardInfo from './CardInfo/CardInfo';
+
+function Card(props) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const { id, title, date } = props.card;
+
+  const formatDate = (value) => {
+    if (!value) return '';
+    const date = new Date().toLocaleDateString('en-GB');
+    if (!date) return '';
+    return date;
+  };
+
+  return (
+    <>
+      {showModal && (
+        <CardInfo
+          onClose={() => setShowModal(false)}
+          card={props.card}
+          boardId={props.boardId}
+          updateCard={props.updateCard}
+          updateDate={props.updateDate}
+        />
+      )}
+      <div
+        className="card"
+        draggable
+        onDragEnd={() => props.dragEnded(props.boardId, id)}
+        onDragEnter={() => props.dragEntered(props.boardId, id)}
+        onClick={() => setShowModal(true)}
+      >
+        <div className="card_top">
+          <div
+            className="card_top_more"
+            onClick={(event) => {
+              event.stopPropagation();
+              setShowDropdown(true);
+            }}
+          >
+            <MoreHorizontal />
+            {showDropdown && (
+              <Dropdown
+                class="board_dropdown"
+                onClose={() => setShowDropdown(false)}
+              >
+                <p onClick={() => props.removeCard(props.boardId, id)}>
+                  Delete Card
+                </p>
+              </Dropdown>
+            )}
+          </div>
+        </div>
+        <div className="card_title">{title}</div>
+        <p>Created on : {props.card.createdDate}</p>
+        {props.card.updatedDate && (
+          <p> Updated on : {props.card.updatedDate}</p>
+        )}
+        <div className="card_footer">
+          {date && <p onChange={() => formatDate(date)}>Due on : {date}</p>}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
---
+++ src/Components/Card/CardInfo/CardInfo.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { Calendar, List, Type } from 'react-feather';
+
+import Modal from '../../Modal/Modal';
+import Editable from '../../Editabled/Editable';
+
+import './CardInfo.css';
+
+function CardInfo(props) {
+  const [values, setValues] = useState({
+    ...props.card,
+  });
+
+  const updateTitle = (value) => {
+    setValues({ ...values, title: value });
+  };
+
+  const updateDesc = (value) => {
+    setValues({ ...values, desc: value });
+  };
+
+  const handleDate = (date) => {
+    if (!date) return;
+
+    setValues({
+      ...values,
+      date,
+    });
+  };
+
+  useEffect(() => {
+    if (props.updateCard) {
+      const updatedDate = props.updateDate();
+      props.updateCard(props.boardId, values.id, values, updatedDate);
+    }
+  }, [values]);
+
+  return (
+    <Modal onClose={props.onClose}>
+      <div className="cardinfo">
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <Type />
+            <p>Title</p>
+          </div>
+          <Editable
+            defaultValue={values.title}
+            text={values.title}
+            placeholder="Enter Title"
+            onSubmit={updateTitle}
+          />
+        </div>
+
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <List />
+            <p>Description</p>
+          </div>
+          <Editable
+            defaultValue={values.desc}
+            text={values.desc || 'Add a Description'}
+            placeholder="Enter description"
+            onSubmit={updateDesc}
+          />
+        </div>
+
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <Calendar />
+            <p>Date</p>
+          </div>
+          <input
+            type="date"
+            defaultValue={values.date}
+            min={new Date().toDateString('en-GB')}
+            onChange={(event) => handleDate(event.target.value)}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default CardInfo;
